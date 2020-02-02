01ff32e818bbc0f4e1cde84ebdbda2efdf410984
hermelap <60103212+hermelap@users.noreply.github.com>
2020-02-01 23:56:22 -0600

    Archivo jupiter y codigo final

--- Laboratio 0.py
+++ Laboratio 0.py
@@ -1,22 +1,24 @@
 # -- ------------------------------------------------------------------------------------ -- #
 # -- Proyecto: Repaso de python 3 y analisis de precios OHLC                              -- #
 # -- Codigo: principal.py - script principal de proyecto                                  -- #
-# -- Rep: https://github.com/ITESOIF/MyST/tree/master/Notas_Python/Notas_RepasoPython     -- #
-# -- Autor: Francisco ME                                                                  -- #
+# -- Rep: https://github.com/hermelap/LAB_0_HPH
+# -- #
+# -- Autor: Hermela Peña                                                              -- #
 # -- ------------------------------------------------------------------------------------ -- #
 
 # -- ------------------------------------------------------------- Importar con funciones -- #
 
-import Funciones as fn                              # Para procesamiento de datos
-import visualizacion as vs                        # Para visualizacion de datos
-import pandas as pd                                 # Procesamiento de datos
-from datos import token as OA_Ak                             # Importar token para API de OANDA
+import Funciones as fn  # Para procesamiento de datos
+import visualizacion as vs  # Para visualizacion de datos
+import pandas as pd  # Procesamiento de datos
+from datos import token as OA_Ak  # Importar token para API de OANDA
+import plotly.graph_objects as go
 
 # -- --------------------------------------------------------- Descargar precios de OANDA -- #
 
 # token de OANDA
-OA_In = "EUR_USD"                  # Instrumento
-OA_Gn = "D"                        # Granularidad de velas
+OA_In = "EUR_USD"  # Instrumento
+OA_Gn = "D"  # Granularidad de velas
 fini = pd.to_datetime("2019-07-06 00:00:00").tz_localize('GMT')  # Fecha inicial
 ffin = pd.to_datetime("2019-12-06 00:00:00").tz_localize('GMT')  # Fecha final
 
@@ -41,7 +43,7 @@ df_pe['hora'] = [df_pe['TimeStamp'][i].hour for i in range(0, len(df_pe['TimeSta
 df_pe['dia'] = [df_pe['TimeStamp'][i].weekday() for i in range(0, len(df_pe['TimeStamp']))]
 
 # -- 0B: Boxplot de amplitud de velas (close - open).
-df_pe['co'] = (df_pe['Close'] - df_pe['Open'])*pip_mult
+df_pe['co'] = (df_pe['Close'] - df_pe['Open']) * pip_mult
 
 # -- ------------------------------------------------------------ Graficar Boxplot plotly -- #
 vs_grafica2 = vs.g_boxplot_varios(p0_data=df_pe[['co']], p1_norm=False)
@@ -51,20 +53,49 @@ vs_grafica2.show()
 df_pe['mes'] = [df_pe['TimeStamp'][i].month for i in range(0, len(df_pe['TimeStamp']))]
 
 # -- 02: Sesion de la vela.
+condiciones = [(lambda i: i == 22 or i == 23 or i == 0 or i == 1 or i == 2 or i == 3 or i == 4 or i == 5 or i == 6
+                          or i == 7, 'asia'),
+               (lambda i: i == 8, 'asia_europa'),
+               (lambda i: i == 9 or i == 10 or i == 11 or i == 12, 'europa'),
+               (lambda i: i == 13 or i == 14 or i == 15 or i == 16, 'europa_america'),
+               (lambda i: i == 17 or i == 18 or i == 19 or i == 20 or i == 21, 'america')]
+
+
+def condiciones2(i):
+    for cond, icion in condiciones:
+        if cond(i):
+            return icion
+    return i
+
+
+df_pe['sesion'] = list(map(condiciones2, df_pe['hora']))
+
 # -- 03: Amplitud OC esperada de vela para cualquier dia de la semana (Dist de Freq).
-df_pe['oc'] = (df_pe['Open'] - df_pe['Close'])*pip_mult
+df_pe['oc'] = (df_pe['Open'] - df_pe['Close']) * pip_mult
 
 # -- 04: Amplitud HL esperada de vela para cualquier dia de la semana (Dist de Freq).
-df_pe['hl'] = (df_pe['High'] - df_pe['Low'])*pip_mult
+df_pe['hl'] = (df_pe['High'] - df_pe['Low']) * pip_mult
 
 # -- 05: Evolucion de velas consecutivas (1: Alcistas, 0: Bajistas).
+df_pe['sentido'] = ['alcista' if df_pe["Close"][i] >= df_pe['Open'][i] else 'bajista'
+                    for i in range(0, len(df_pe['Open']))]
+
 # -- 06: Maxima evolucion esperada de velas consecutivas (Dist Acum de Freq).
-# -- 07: Calculo + Grafica autopropuesta.
-# -- Ventanas moviles de volatilidad
-x=10
-df_pe['volatilidad_5'] = df_pe.iloc[:,x].rolling(window=5).mean()
-df_pe['volatilidad_25'] =df_pe.iloc[:,x].rolling(window=5).mean()
-df_pe['volatilidad_50'] = df_pe.iloc[:,x].rolling(window=5).mean()
+df_pe['sentido_c'] = 0
+x = 0
+for i in range(1, len(df_pe['sentido_c'])):
+    if df_pe['sentido'][i] == df_pe['sentido'][i - 1]:
+        x = x + 1
+        df_pe.loc[i, 'sentido_c'] = x
+    else:
+        x = 0
+        df_pe.loc[i, 'sentido_c'] = x
+
+# -- 07: Ventanas moviles de volatilidad
+x = df_pe.columns.get_loc('hl')
+df_pe['volatilidad_5'] = df_pe.iloc[:, x].rolling(window=5).mean()
+df_pe['volatilidad_25'] = df_pe.iloc[:, x].rolling(window=25).mean()
+df_pe['volatilidad_50'] = df_pe.iloc[:, x].rolling(window=50).mean()
 
 # -- Gráfica plotly
 # --  Tiene que tener título de gráfica
@@ -72,5 +103,8 @@ df_pe['volatilidad_50'] = df_pe.iloc[:,x].rolling(window=5).mean()
 # --  Tiene que tener título de eje y y etiquetas de eje y
 # --  Se debe de poder visualizar una leyenda (en cualquier posición).
 
-
-
+vs_grafica3 = go.Figure()
+vs_grafica3.add_trace(go.Histogram(x=df_pe['sentido_c']))
+vs_grafica3.update_layout(title={'text': "Distribución conteo velas consecutivas"}, xaxis_title='repeticiones',
+                          yaxis_title='frecuencia')
+vs_grafica3.show()
